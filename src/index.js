2920f7a7766ac501339cd3b06b94fa417e144c7a
wati2 <yt.wati@gmail.com>
2020-06-22 20:05:43 +0900

    1st

--- src/index.js
+++ src/index.js
@@ -4,12 +4,35 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+
+function formatName(user) {
+  return user.firstName + " " + user.lastName;
+}
+
+const user = {
+  firstName: '김',
+  lastName: '소윤'
+};
+
+// 어트리뷰트에 Javascript 표현식을 삽입할 때 중괄호 주변에 따옴표를 입력하지 마세요. 따옴표 또는 중괄호중 하나만 사용하고, 동일한 어트리뷰트에 두 가지를 동시에 사용하면 안됩니다.
+
+const element = <h1>Hello, {formatName(user)}</h1>;
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  element,
   document.getElementById('root')
 );
+
+
+
+
+
+
+
+
+
+
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
