0ae4ac8566fec9ddbf1757ece790fedc5f7d2dce
wati2 <yt.wati@gmail.com>
2020-06-22 20:07:52 +0900

    Time

--- src/index.js
+++ src/index.js
@@ -5,28 +5,17 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 
-function formatName(user) {
-  return user.firstName + " " + user.lastName;
+function tick() {
+  const element = (
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
+  ReactDOM.render(element, document.getElementById('root'));
 }
 
-const user = {
-  firstName: '김',
-  lastName: '소윤'
-};
-
-// 어트리뷰트에 Javascript 표현식을 삽입할 때 중괄호 주변에 따옴표를 입력하지 마세요. 따옴표 또는 중괄호중 하나만 사용하고, 동일한 어트리뷰트에 두 가지를 동시에 사용하면 안됩니다.
-
-const element = <h1>Hello, {formatName(user)}</h1>;
-
-ReactDOM.render(
-  element,
-  document.getElementById('root')
-);
-
-
-
-
-
+setInterval(tick, 1000);
 
 
 
