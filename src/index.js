a0fbcfb96b602c1020a8edcd49e7cf803bfe543f
wati2 <yt.wati@gmail.com>
2020-06-23 19:30:08 +0900

    6. Docs 이벤트 처리하기

--- src/index.js
+++ src/index.js
@@ -4,65 +4,39 @@ import './index.css';
 // import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-function FormattedDate(props) {
-return <h2> It is {props.date.toLocaleTimeString()}.</h2>;
-}
 
-
-class Clock extends React.Component {
-  // 생성자의 존재이유 = 초기값 설정
-  constructor(props){
+class Toggle extends React.Component {
+  // 생성자는 위에서 받아오고 생성하는 순간의 state를 정의하기 위한 도구이다
+  constructor(props) {
     super(props);
-    this.state = {date:new Date()};
+    this.state = {isToggleOn: true};
+
+    // This binding is necessary to make `this` work in the callback
+    // JSX 콜백 안에서 this의 의미에 대해 주의해야 합니다. JavaScript에서 클래스 메서드는 기본적으로 바인딩되어 있지 않습니다. this.handleClick을 바인딩하지 않고 onClick에 전달하였다면, 함수가 실제 호출될 때 this는 undefined가 됩니다.
+
+    // 이는 React만의 특수한 동작이 아니며, JavaScript에서 함수가 작동하는 방식의 일부입니다. 일반적으로 onClick={this.handleClick}과 같이 뒤에 ()를 사용하지 않고 메서드를 참조할 경우, 해당 메서드를 바인딩 해야 합니다.
+    this.handleClick = this.handleClick.bind(this);
   }
 
-  componentDidMount() {
-    this.timerID = setInterval(
-      () => this.tick(),
-      1000
-    );
-  }
-  
-  componentWillUnmount() {
-    clearInterval(this.timerID);
-  }
-
-  tick() {
-    this.setState({
-      date: new Date()
-    });
+  handleClick(){
+    this.setState(prevState => ({
+      isToggleOn: !prevState.isToggleOn
+    }));
   }
 
   render() {
     return (
-      <div>
-        <h1>Hello, world</h1>
-        <FormattedDate date={this.state.date} />
-      </div>
+      <button onClick={this.handleClick}>
+        {this.state.isToggleOn ? 'On' : 'OFF'}
+      </button>
     );
   }
 }
 
-function App() {
-  return(
-    <div>
-      <Clock />
-      <Clock />
-      <Clock />
-    </div>
-  )
-}
-
-
 ReactDOM.render(
-  // OR <App />
-  <div>
-    <Clock />
-    <Clock />
-    <Clock />
-  </div>,
+  <Toggle />,
   document.getElementById('root')
-);
+)
 
 
 
