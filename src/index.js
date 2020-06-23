d66fda37139254bedbf434a176f43ec7262e72a7
wati2 <yt.wati@gmail.com>
2020-06-23 19:41:46 +0900

    7-1 조건부렌더링

--- src/index.js
+++ src/index.js
@@ -1,42 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+function UserGreeting(props) {
+  return <h1>Welcome back</h1>;
+}
 
-class Toggle extends React.Component {
-  // 생성자는 위에서 받아오고 생성하는 순간의 state를 정의하기 위한 도구이다
-  constructor(props) {
-    super(props);
-    this.state = {isToggleOn: true};
+function GuestGreeting(props) {
+  return <h1>Please sign up.</h1>;
+}
 
-    // This binding is necessary to make `this` work in the callback
-    // JSX 콜백 안에서 this의 의미에 대해 주의해야 합니다. JavaScript에서 클래스 메서드는 기본적으로 바인딩되어 있지 않습니다. this.handleClick을 바인딩하지 않고 onClick에 전달하였다면, 함수가 실제 호출될 때 this는 undefined가 됩니다.
-
-    // 이는 React만의 특수한 동작이 아니며, JavaScript에서 함수가 작동하는 방식의 일부입니다. 일반적으로 onClick={this.handleClick}과 같이 뒤에 ()를 사용하지 않고 메서드를 참조할 경우, 해당 메서드를 바인딩 해야 합니다.
-    this.handleClick = this.handleClick.bind(this);
+function Greeting(props) {
+  const isLoggedIn = props.isLoggedIn;
+  if (isLoggedIn) {
+    return <UserGreeting />;
   }
-
-  handleClick(){
-    this.setState(prevState => ({
-      isToggleOn: !prevState.isToggleOn
-    }));
-  }
-
-  render() {
-    return (
-      <button onClick={this.handleClick}>
-        {this.state.isToggleOn ? 'On' : 'OFF'}
-      </button>
-    );
-  }
+  return <GuestGreeting />;
 }
 
 ReactDOM.render(
-  <Toggle />,
+  // Try changing to isLoggedIn={true}:
+  <Greeting isLoggedIn={true} />,
   document.getElementById('root')
-)
+);
+
 
 
 
