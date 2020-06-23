4835ce629504832e58b45cdd9c69d2249cc18180
wati2 <yt.wati@gmail.com>
2020-06-23 19:59:29 +0900

    7-1 조건부렌더링

--- src/index.js
+++ src/index.js
@@ -3,12 +3,48 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 
+
+
+class LoginControl extends React.Component {
+  constructor(props){
+    super(props);
+    this.handleLoginClick = this.handleLoginClick.bind(this);
+    this.handleLogoutClick = this.handleLogoutClick.bind(this);
+    this.state = {isLoggedIn: false};
+  }
+  
+  handleLoginClick() {
+    this.setState({isLoggedIn: true});
+  }
+  
+  handleLogoutClick() {
+    this.setState({isLoggedIn: false});
+  }
+  
+  render() {
+    const isLoggedIn = this.state.isLoggedIn;
+    let button;
+    if (isLoggedIn) {
+      button = <LogoutButton onClick={this.handleLogoutClick} />;
+    } else {
+      button = <LoginButton onClick={this.handleLoginClick} />;
+    }
+    
+    return (
+      <div>
+        <Greeting isLoggedIn={isLoggedIn} />
+        {button}
+      </div>
+    )
+  }
+}
+
 function UserGreeting(props) {
-  return <h1>Welcome back</h1>;
+  return <h1>Welcome back!</h1>;
 }
 
 function GuestGreeting(props) {
-  return <h1>Please sign up.</h1>;
+  return <h1>Please sign up.</h1>
 }
 
 function Greeting(props) {
@@ -19,15 +55,28 @@ function Greeting(props) {
   return <GuestGreeting />;
 }
 
+function LoginButton(props) {
+  return (
+    <button onClick={props.onClick}>
+      Login
+    </button>
+  );
+}
+
+function LogoutButton(props) {
+  return (
+    <button onClick={props.onClick}>
+      Logout
+    </button>
+  );
+}
+
+
+
 ReactDOM.render(
-  // Try changing to isLoggedIn={true}:
-  <Greeting isLoggedIn={true} />,
+  <LoginControl />,
   document.getElementById('root')
 );
-
-
-
-
 
 
 // If you want your app to work offline and load faster, you can change
