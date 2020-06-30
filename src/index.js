dc5a7518941ce2456cb885b6f8add1b0f4efe4f9
wati2 <yt.wati@gmail.com>
2020-06-30 19:47:12 +0900

    조건부 렌더링 true && expression

--- src/index.js
+++ src/index.js
@@ -5,76 +5,27 @@ import * as serviceWorker from './serviceWorker';
 
 
 
-class LoginControl extends React.Component {
-  constructor(props){
-    super(props);
-    this.handleLoginClick = this.handleLoginClick.bind(this);
-    this.handleLogoutClick = this.handleLogoutClick.bind(this);
-    this.state = {isLoggedIn: false};
-  }
-  
-  handleLoginClick() {
-    this.setState({isLoggedIn: true});
-  }
-  
-  handleLogoutClick() {
-    this.setState({isLoggedIn: false});
-  }
-  
-  render() {
-    const isLoggedIn = this.state.isLoggedIn;
-    let button;
-    if (isLoggedIn) {
-      button = <LogoutButton onClick={this.handleLogoutClick} />;
-    } else {
-      button = <LoginButton onClick={this.handleLoginClick} />;
-    }
-    
-    return (
-      <div>
-        <Greeting isLoggedIn={isLoggedIn} />
-        {button}
-      </div>
-    )
-  }
-}
-
-function UserGreeting(props) {
-  return <h1>Welcome back!</h1>;
-}
-
-function GuestGreeting(props) {
-  return <h1>Please sign up.</h1>
-}
-
-function Greeting(props) {
-  const isLoggedIn = props.isLoggedIn;
-  if (isLoggedIn) {
-    return <UserGreeting />;
-  }
-  return <GuestGreeting />;
-}
-
-function LoginButton(props) {
+function Mailbox(props) {
+  const unreadMessages = props.unreadMessages;
   return (
-    <button onClick={props.onClick}>
-      Login
-    </button>
+    <div>
+      <h1>Hello!</h1>
+      {unreadMessages.length > 0 &&
+        <h2>
+          You have {unreadMessages.length} unread messages.
+        </h2>
+      }
+    </div>
   );
 }
 
-function LogoutButton(props) {
-  return (
-    <button onClick={props.onClick}>
-      Logout
-    </button>
-  );
-}
+const messages = ['React', 'Re: React', 'Re:Re: React'];
 
-
+// 개쩜.. Javascript에서 true && expression은 항상 expression으로 평가되고
+// false && expression은 항상 false로 평가됩니다.
 
 ReactDOM.render(
-  <LoginControl />,
+  <Mailbox unreadMessages={messages} />,
   document.getElementById('root')
 );
 
