0a08b4febb2d05117c1e3cea4652946afcb3b172
wati2 <yt.wati@gmail.com>
2020-06-30 19:59:25 +0900

    8. list - 여러개의 컴포넌트 렌더링하기

--- src/index.js
+++ src/index.js
@@ -5,48 +5,15 @@ import * as serviceWorker from './serviceWorker';
 import { findRenderedDOMComponentWithClass } from 'react-dom/test-utils';
 
 
-
-function WarningBanner(props) {
-  if (!props.warn) {
-    return null;
-  }
-
-  return (
-    <div className="warning">
-      Warning!
-    </div>
-  );
-}
-
-class Page extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {showWarning: true}
-    this.handleToggleClick = this.handleToggleClick.bind(this);
-  }
-
-  handleToggleClick() {
-    this.setState(prevState => ({
-      showWarning: !prevState.showWarning
-    }));
-  }
-
-  render() {
-    return (
-      <div>
-        <WarningBanner warn={this.state.showWarning} />
-        <button onClick={this.handleToggleClick}>
-          {this.state.showWarning ? '보이냐?클릭해봐라' : '안보이냐?클릭해봐라'}
-        </button>
-      </div>
-    );
-  }
-}
+const numbers = [1,2,3,4,5];
+const listItems = numbers.map((numbers) =>
+  <li>{numbers}</li>
+);
 
 ReactDOM.render(
-  <Page />,
+  <ul>{listItems}</ul>,
   document.getElementById('root')
-);
+)
 
 
 // If you want your app to work offline and load faster, you can change
