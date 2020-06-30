e93c8524f9eecf363fd9273f1d1e508e32536c81
wati2 <yt.wati@gmail.com>
2020-06-30 20:26:40 +0900

    예시 올바른 key의 사용법

--- src/index.js
+++ src/index.js
@@ -2,18 +2,33 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-import { findRenderedDOMComponentWithClass } from 'react-dom/test-utils';
+// import { findRenderedDOMComponentWithClass } from 'react-dom/test-utils';
 
+
+function ListItem(props){
+  // 맞습니다! 여기에는 key를 지정할 필요가 없습니다.
+  return <li>{props.value}</li>;
+}
+
+function NumberList(props) {
+  const numbers = props.numbers;
+  const listItems = numbers.map((number) =>
+    // 맞습니다! 배열 안에 key를 지정해야 합니다.
+    <ListItem key={number.toString()} value={number} />
+  );
+  return (
+    <ul>
+      {listItems}
+    </ul>
+  );
+}
 
 const numbers = [1,2,3,4,5];
-const listItems = numbers.map((numbers) =>
-  <li>{numbers}</li>
-);
 
 ReactDOM.render(
-  <ul>{listItems}</ul>,
+  <NumberList numbers={numbers} />,
   document.getElementById('root')
-)
+);
 
 
 // If you want your app to work offline and load faster, you can change
