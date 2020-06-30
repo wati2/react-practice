b365c35a7f9b561d4d5178588072938232fae6bb
wati2 <yt.wati@gmail.com>
2020-06-30 21:31:24 +0900

    9. 나는 오타머신이다

--- src/index.js
+++ src/index.js
@@ -5,35 +5,46 @@ import * as serviceWorker from './serviceWorker';
 // import { findRenderedDOMComponentWithClass } from 'react-dom/test-utils';
 
 
-class FlavorForm extends React.Component {
+class Reservation extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {value: 'coconut'};
+    this.state = {
+      isGoing: true,
+      numberOfGuests: 2
+    };
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
   }
 
-  handleChange(event) {
-    this.setState({vaule: event.target.value});
-  }
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
 
-  handleSubmit(event){
-    alert('Yout gavorite flavor is: ' + this.state.value);
-    event.preventDefailt();
+    this.setState({
+      [name]: value
+    });
   }
 
   render() {
     return (
-      <form onSubmit = {this.hadleSubmit}>
+      <form>
         <label>
-          Pick your favorite flavor:
-          <select value={this.state.value} onChange={this.handleChange}>
-            <option value="grapefruit">Grapefruit</option>
-            <option value="lime">Lime</option>
-            <option value="coconut">Coconut</option>
-            <option value="mango">Mango</option>
-          </select>
+          Is going:
+          <input
+            name="isGoing"
+            type="checkbox"
+            checked={this.state.isGoing}
+            onChange={this.handleInputChange} />
+        </label>
+        <br />
+        <label>
+          Number of guests:
+          <input
+            name="numberOfGuests"
+            type="number"
+            value={this.state.numberOfGuests}
+            onChange={this.handleInputChange} />
         </label>
       </form>
     );
@@ -41,13 +52,9 @@ class FlavorForm extends React.Component {
 }
 
 ReactDOM.render(
-  <FlavorForm />,
+  <Reservation />,
   document.getElementById('root')
 );
-
-
-
-
 
 
 
