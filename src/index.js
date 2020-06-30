8de3c1829a5160f1848c7a9c62d6701318f32474
wati2 <yt.wati@gmail.com>
2020-06-30 21:00:09 +0900

    9.폼 > Select TAG

--- src/index.js
+++ src/index.js
@@ -5,41 +5,51 @@ import * as serviceWorker from './serviceWorker';
 // import { findRenderedDOMComponentWithClass } from 'react-dom/test-utils';
 
 
-class NameForm extends React.Component {
+class FlavorForm extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {value: '헬로'};
+    this.state = {value: 'coconut'};
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChange(event) {
-    this.setState({value: event.target.value});
+    this.setState({vaule: event.target.value});
   }
 
-  handleSubmit(event) {
-    alert('A name was submitted: ' + this.state.value);
-    event.preventDefault();
+  handleSubmit(event){
+    alert('Yout gavorite flavor is: ' + this.state.value);
+    event.preventDefailt();
   }
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
+      <form onSubmit = {this.hadleSubmit}>
         <label>
-          Name:
-          <input type="text" value={this.state.value} onChange={this.handleChange} />
+          Pick your favorite flavor:
+          <select value={this.state.value} onChange={this.handleChange}>
+            <option value="grapefruit">Grapefruit</option>
+            <option value="lime">Lime</option>
+            <option value="coconut">Coconut</option>
+            <option value="mango">Mango</option>
+          </select>
         </label>
-        <input type="submit" value="Submit" />
       </form>
     );
   }
 }
 
 ReactDOM.render(
-  <NameForm />,
+  <FlavorForm />,
   document.getElementById('root')
 );
+
+
+
+
+
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
