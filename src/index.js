6f9d43a950dd69238958caa52169314654e66ca1
wati2 <yt.wati@gmail.com>
2020-07-01 23:57:55 +0900

    11 - ~~~

--- src/index.js
+++ src/index.js
@@ -29,9 +29,42 @@ function Dialog(props) {
       <p className="Dialog-message">
         {props.message}
       </p>
+      {props.children}
     </FancyBorder>
   )
 }
+
+class SignUpDialog extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSignUp = this.handleSignUp.bind(this);
+    this.state = {login: ''};
+  }
+
+  render() {
+    return (
+      <Dialog title="Mars Exploration Program"
+              message="How should we refer to you?">
+        <input value={this.state.login}
+               onChange={this.handleChange} />
+        <button onClick={this.handleSignUp}>
+          Sign Me Up!
+        </button>
+      </Dialog>
+    )
+  }
+
+  handleChange(e) {
+    this.setState({login: e.target.value});
+  }
+
+  handleSignUp() {
+    alert(`Welcome aboard, ${this.state.login}!`);
+  }
+}
+
+
 
 function WelcomeDialog(){
   return (
@@ -69,7 +102,7 @@ function App() {
 
 
 ReactDOM.render(
-  <WelcomeDialog />,
+  <SignUpDialog />,
   document.getElementById('root')
 )
 
